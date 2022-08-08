5125a0d48999665d7b4762cce495575d2afe0d66
Nicholas Jedamzik <n.jedamzik@jedamzik-jedamzik.de>
2022-08-08 17:42:53 +0200

    provide compatibility for Laravel 9
    * update league/commonmark to ^2.3.5
    * drop support for statamic 3.2
    * support Laravel 9

--- src/Extensions/CommonMark/TransformHeadingsToListOfLinksProcessor.php
+++ src/Extensions/CommonMark/TransformHeadingsToListOfLinksProcessor.php
@@ -2,14 +2,14 @@
 
 namespace Njed\Toc\Extensions\CommonMark;
 
-use League\CommonMark\Block\Element\Heading;
-use League\CommonMark\Block\Element\ListBlock;
-use League\CommonMark\Block\Element\ListData;
-use League\CommonMark\Block\Element\ListItem;
-use League\CommonMark\Block\Element\Paragraph;
+use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
+use League\CommonMark\Extension\CommonMark\Node\Block\ListBlock;
+use League\CommonMark\Extension\CommonMark\Node\Block\ListData;
+use League\CommonMark\Extension\CommonMark\Node\Block\ListItem;
+use League\CommonMark\Node\Block\Paragraph;
 use League\CommonMark\Event\DocumentParsedEvent;
-use League\CommonMark\Inline\Element\Link;
-use League\CommonMark\Inline\Element\Text;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Link;
+use League\CommonMark\Node\Inline\Text;
 
 final class TransformHeadingsToListOfLinksProcessor
 {
@@ -22,22 +22,25 @@ final class TransformHeadingsToListOfLinksProcessor
         $data->type = ListBlock::TYPE_BULLET;
 
         $listBlock = new ListBlock($data);
-        $listBlock->data['attributes']['class'] = 'table-of-contents';
+        $listBlock->data->set('attributes', [
+            'class' => 'table-of-contents',
+        ]);
 
         while ($event = $walker->next()) {
             $node = $event->getNode();
             if ($event->isEntering() && $node instanceof Heading && ($node->getLevel() <= 2 || $this->configIncludesLevel($node->getLevel()))) {
+                $content = $node->lastChild()->getLiteral();
                 if (!config('toc.anchorLinks', false) ||
-                    !$node->getData('attributes') ||
-                    !array_key_exists('id', $node->getData('attributes')) ||
-                    is_null($node->data['attributes']['id'])) {
+                    !$node->data->get('attributes') ||
+                    !array_key_exists('id', $node->data->get('attributes')) ||
+                    is_null($node->data->get('attributes')['id'])) {
                     $paragraph = new Paragraph;
-                    $paragraph->appendChild(new Text($node->getStringContent()));
+                    $paragraph->appendChild(new Text($content));
                     $listItem = new ListItem($data);
                     $listItem->appendChild($paragraph);
                 } else {
-                    $fragment = $node->getData('attributes')['id'];
-                    $linkNode = new Link("#{$fragment}", $node->getStringContent(), $node->getStringContent());
+                    $fragment = $node->data->get('attributes')['id'];
+                    $linkNode = new Link("#{$fragment}", $content, $content);
 
                     $paragraph = new Paragraph;
                     $paragraph->appendChild($linkNode);
@@ -45,7 +48,9 @@ final class TransformHeadingsToListOfLinksProcessor
                     $listItem->appendChild($paragraph);
                 }
 
-                $listItem->data['attributes']['class'] = $this->getNodeClasses($node->getLevel());
+                $listItem->data->set('attributes', [
+                    'class' => $this->getNodeClasses($node->getLevel())
+                ]);
 
                 $listBlock->appendChild($listItem);
             }
